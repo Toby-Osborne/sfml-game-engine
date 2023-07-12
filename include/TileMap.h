6ad53c6287bd808522a9f9cafa4eec39fcca532e
TOBY OSBORNE <tosb789@aucklanduni.ac.nz>
2023-07-12 16:24:02 +1200

    implemented basic tilemap example to build from

---
+++ include/TileMap.h
@@ -0,0 +1,19 @@
+#ifndef CPPGAME_TEST_TILEMAP_H
+#define CPPGAME_TEST_TILEMAP_H
+
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+
+class TileMap : public sf::Drawable, public sf::Transformable{
+public:
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
+
+#endif //CPPGAME_TEST_TILEMAP_H
