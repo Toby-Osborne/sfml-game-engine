2d4126c61cd7856fa60ff97f45ee3d644c7e5e5a
tazukiswift <tosb789@aucklanduni.ac.nz>
2023-07-19 21:00:43 +1200

    updated player physics

--- include/Player.h
+++ include/Player.h
@@ -8,8 +8,6 @@ class Player {
 public:
     const sf::Vector2f get_player_coordinates();
 
-    void set_player_coordinates(sf::Vector2f);
-
     explicit Player(std::shared_ptr<Map> map);  // Map loaded on launch
 
     // This is a terrible solution because if player goes out of scope then its over
@@ -18,7 +16,7 @@ public:
     void process_player(const sf::Vector2f &movement_input);
 
 private:
-    sf::Vector2f _player_coordinates = sf::Vector2f(10000, 10000);
+    sf::Vector2f _player_coordinates = sf::Vector2f(2000, 2000);
     std::unique_ptr<MapRender> _render_map;
     std::shared_ptr<Map> _map;
     sf::Clock _player_clock;
@@ -27,10 +25,10 @@ private:
     // Scuffed Physics implementation?
     float mass = 1.0;
     float inverse_mass = 1 / mass;
-    float acceleration_coefficient = 1000.0;
-    float max_velocity = 500;
+    float acceleration_coefficient = 3000.0;
+    float max_velocity = 1000;
     sf::Vector2f velocity = sf::Vector2f(0.f, 0.f);
-    static constexpr int vel_stop_threshold = 20.0;
+    static constexpr int vel_stop_threshold = 100.0;
 
     void _handle_player_physics(const sf::Vector2f &movement_input);
 };
